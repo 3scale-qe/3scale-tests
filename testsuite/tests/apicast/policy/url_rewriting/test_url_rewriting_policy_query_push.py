acafb804f051f21d6628c4b5e94c7b458a2a434e
David Rajnoha <drajnoha@redhat.com>
2020-03-27 10:19:51 +0000

    Added the test_url_rewriting_policy_query_push, updated the documentation at test_url_rewriting_policy_add

--- testsuite/tests/apicast/policy/url_rewriting/test_url_rewriting_policy_query_push.py
+++ testsuite/tests/apicast/policy/url_rewriting/test_url_rewriting_policy_query_push.py
@@ -1,5 +1,5 @@
 """
-Rewrite spec/functional_specs/policies/url_rewrite_query/query_rewrite_policy_add_spec.rb
+Rewrite spec/functional_specs/policies/url_rewrite_query/query_rewrite_policy_push_spec.rb
 """
 
 import pytest
@@ -11,18 +11,18 @@ from testsuite.echoed_request import EchoedRequest
 @pytest.fixture(scope="module")
 def policy_settings():
     """
-    Add the 'url_rewriting' policy to policy chain with following settings
+    Add the 'url_rewriting' policy to the policy chain with following settings
     """
     return rawobj.PolicyConfig("url_rewriting", {
-        "query_args_commands": [{"op": "add", "arg": "new_arg", "value": "new_value"},
-                                {"op": "add", "arg": "arg", "value": "value"}]})
+        "query_args_commands": [{"op": "push", "arg": "new_arg", "value": "new_value"},
+                                {"op": "push", "arg": "arg", "value": "value"}]})
 
 
-def test_url_rewriting_add_query_args(application):
+def test_url_rewriting_push_query_args(application):
     """
     Make request with parameter 'arg' with value 'value'
     Check if the 'value' has been added to the 'arg' parameter
-    Check if parameter 'new_arg' has not been created
+    Check if new parameter 'new_arg' has been created with the 'new_value' value
     """
     client = application.api_client()
     response = client.get("/get", params={"arg": "old_value"})
@@ -30,4 +30,4 @@ def test_url_rewriting_add_query_args(application):
 
     assert response.status_code == 200
     assert echoed_request.params.get('arg') == ['old_value', 'value']
-    assert echoed_request.params.get('new_arg') != 'new_value'
+    assert echoed_request.params.get('new_arg') == 'new_value'
