898a91348a3523b497cb5a85b241ddf5a0784587
Jakub Smadiš <jsmadis@redhat.com>
2020-04-20 12:08:21 +0200

    Routing test for apiap + regression test for THREESCALE-4937

---
+++ testsuite/tests/apicast/apiap/routing/routing_cases.py
@@ -0,0 +1,32 @@
+"""
+Test cases that are specifying paths that are used to connect product with backend
+
+There are cases that are regression test for:
+    https://issues.redhat.com/browse/THREESCALE-4937
+"""
+
+
+def path_valid_chars():
+    """Path valid characters that are used in test cases"""
+    return ["-", "_", "~", "!", "&", ",", ";", "=", "@", "/"]
+
+
+def case_default_get():
+    """Default test that checks routing with /get path"""
+    return ['/get']
+
+
+def case_url_valid_chars():
+    """
+    Array of paths '/foo{p}bar', where p is each char of path valid characters
+    Regression: THREESCALE-4937
+    """
+    return [f"/foo{char}bar" for char in path_valid_chars()]
+
+
+def case_url_valid_chars_in_two_directories():
+    """
+    Array of paths '/foo{p}bar/foo{p}bar', where p is each char of path valid characters
+    Regression: THREESCALE-4937
+    """
+    return [f"/foo{char}bar/foo{char}bar" for char in path_valid_chars()]
---
+++ testsuite/tests/apicast/apiap/routing/test_routing.py
@@ -0,0 +1,65 @@
+"""
+Test that request to product with specific paths to backends will be routed to correct backend
+"""
+import pytest
+from pytest_cases import fixture_plus, cases_data
+
+from testsuite import rawobj
+from testsuite.echoed_request import EchoedRequest
+from testsuite.tests.apicast.apiap.routing import routing_cases
+from testsuite.utils import blame
+
+pytestmark = pytest.mark.skipif("TESTED_VERSION < Version('2.8.2')")
+
+
+@fixture_plus
+def service(backends_mapping, service_proxy_settings, custom_service, request, lifecycle_hooks):
+    """Service configured with all backends that are connected via each path from paths"""
+    return custom_service({"name": blame(request, "svc")},
+                          service_proxy_settings,
+                          backends_mapping,
+                          hooks=lifecycle_hooks)
+
+
+@fixture_plus
+@cases_data(module=routing_cases)
+def paths(case_data):
+    """Array of paths that are used for backend usages (for each path connect product with new backend)"""
+    return case_data.get()
+
+
+@fixture_plus
+def backends_mapping(custom_backend, paths, private_base_url):
+    """
+    Backend mapping for each path is created new backend
+    Backend url is created with suffix "/anything{i}", where i is index of path in array paths
+        in order to test that request was routed to correct backend
+    """
+    mappings = {}
+    for i, path in enumerate(paths):
+        url = private_base_url() + f"/anything/{i}"
+        mappings.update({path: custom_backend(endpoint=url)})
+    return mappings
+
+
+@fixture_plus
+def application(service, custom_app_plan, custom_application, lifecycle_hooks, request):
+    """application bound to the account and service"""
+    plan = custom_app_plan(rawobj.ApplicationPlan(blame(request, "aplan")), service)
+    return custom_application(rawobj.Application(blame(request, "app"), plan), hooks=lifecycle_hooks)
+
+
+def test(application, paths):
+    """
+    Test that  for each path from paths, the request will be routed to correct backend.
+    Each backend is connected to product via 'path' and has url that has suffix "/anything{i}"
+        in order to know that it was routed to correct backend
+    :param application: application
+    :param paths: array of used paths
+    """
+    for i, path in enumerate(paths):
+        response = application.api_client().get(f"{path}")
+        assert response.status_code == 200, f"Path {path} was not mapped to any backend"
+
+        echoed_request = EchoedRequest.create(response)
+        assert echoed_request.path == f"/anything/{i}/", f"Path {path} was routed incorrectly"
