03fb67f3e965864ed0f0f436556f52d5a8325bc8
Jakub Smadiš <jsmadis@redhat.com>
2020-05-07 16:00:11 +0200

    Content caching test with apiap

---
+++ testsuite/tests/apicast/policy/content_caching/test_content_caching_apiap.py
@@ -0,0 +1,142 @@
+"""
+Test valid content caching on product with multiple backends
+"""
+import pytest
+
+from testsuite import rawobj
+from testsuite.echoed_request import EchoedRequest
+from testsuite.utils import randomize, blame
+
+
+@pytest.fixture(scope="module")
+def policy_settings():
+    "config of cors policy"
+    return rawobj.PolicyConfig("content_caching", {
+        "rules": [{
+            "cache": True,
+            "header": "X-Cache-Status",
+            "condition": {
+                "combine_op": "and",
+                "operations": [{
+                    "left": "oo",
+                    "op": "==",
+                    "right": "oo"
+                }]
+            }
+        }]
+    })
+
+
+@pytest.fixture(scope="module")
+def backends_mapping(custom_backend, private_base_url):
+    """
+    Create 2 separate backends:
+        - path to Backend 1: "/echo-api"
+        - path to Backend 2: "/httpbin"
+    """
+    return {"/echo-api": custom_backend("backend_one", endpoint=private_base_url("echo-api")),
+            "/httpbin": custom_backend("backend_two", endpoint=private_base_url("httpbin"))}
+
+
+# pylint: disable=too-many-arguments
+@pytest.fixture(scope="module")
+def service2(backends_mapping, custom_service, service_proxy_settings, policy_settings, lifecycle_hooks, request):
+    """Second service to test with"""
+    svc = custom_service({"name": blame(request, "svc")}, service_proxy_settings, backends_mapping,
+                         hooks=lifecycle_hooks)
+    svc.proxy.list().policies.append(policy_settings)
+    return svc
+
+
+@pytest.fixture(scope="module")
+def application2(service2, custom_application, custom_app_plan, lifecycle_hooks):
+    """Second application to test with"""
+    plan = custom_app_plan(rawobj.ApplicationPlan(randomize("AppPlan")), service2)
+    return custom_application(rawobj.Application(randomize("App"), plan), hooks=lifecycle_hooks)
+
+
+@pytest.fixture(scope="module")
+def api_client(api_client):
+    """
+    Api client for the first service
+    Apicast needs to load configuration in order to cache incomming requests
+    """
+    api_client.get("/echo-api/")
+    return api_client
+
+
+@pytest.fixture(scope="module")
+def api_client2(application2):
+    """
+    Api client for the second service
+    Apicast needs to load configuration in order to cache incomming requests
+    """
+    client = application2.api_client()
+    client.get("/echo-api/")
+    return client
+
+
+@pytest.mark.parametrize('client', ('api_client', 'api_client2'), ids=["First service", "Second service"])
+def test_caching_working_correctly(request, client):
+    """
+    Test that cache on works correctly with APIAP
+    """
+    api_client = request.getfixturevalue(client)
+
+    response = api_client.get("/echo-api/working", headers=dict(origin="localhost"))
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") != "HIT"
+
+    for i in range(10):
+        response = api_client.get("/echo-api/working", headers=dict(origin="localhost"))
+        assert response.status_code == 200, "Request {} failed".format(i)
+        assert response.headers.get("X-Cache-Status") == "HIT", "Request {} didn't hit the cache".format(i)
+
+
+def test_other_service_cache(api_client, api_client2):
+    """
+    Test that cache of one product will not be used on the request of another product to the same backend
+    """
+    response = api_client.get("/echo-api/test", headers=dict(origin="localhost"))
+    echoed_request = EchoedRequest.create(response)
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") != "HIT"
+
+    response = api_client.get("/echo-api/test", headers=dict(origin="localhost"))
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") == "HIT"
+
+    # another service should not hit the cache on same path and same backend
+    response = api_client2.get("/echo-api/test", headers=dict(origin="localhost"))
+    echoed_request2 = EchoedRequest.create(response)
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") != "HIT"
+
+    # Body of each response should be different
+    assert echoed_request.json['uuid'] != echoed_request2.json['uuid']
+
+    # Request to first service should be still cached
+    response = api_client.get("/echo-api/test", headers=dict(origin="localhost"))
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") == "HIT"
+
+
+def test_other_backend_cache(api_client):
+    """Test that cache of one backend will not be used on the request to another backend with the same path"""
+    response = api_client.get("/echo-api/anything/test", headers=dict(origin="localhost"))
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") != "HIT"
+
+    response = api_client.get("/echo-api/anything/test", headers=dict(origin="localhost"))
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") == "HIT"
+
+    # another backend should not hit the cache of other backend on the same path
+    response = api_client.get("/httpbin/anything/test", headers=dict(origin="localhost"))
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") != "HIT"
+
+    # Request to first backend should be still cached
+    response = api_client.get("/echo-api/anything/test", headers=dict(origin="localhost"))
+    assert response.status_code == 200
+    assert response.headers.get("X-Cache-Status") == "HIT"
