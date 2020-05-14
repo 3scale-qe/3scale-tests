fc5456ccbe304e40d7984ac135fedf28ec6fb7b8
Hchrast <hchrast@redhat.com>
2020-05-14 11:41:41 +0200

    Rewrite spec/functional_specs/proxy_endpoints_set_spec.rb

---
+++ testsuite/tests/system/test_proxy_endpoints_set_spec.py
@@ -0,0 +1,18 @@
+"""
+Rewrite spec/functional_specs/proxy_endpoints_set_spec.rb
+"""
+
+
+def test_proxy_endpoints_set(service, configuration):
+    """
+    Test checks if the endpoints match.
+    """
+    service_name = service.entity["name"]
+    tenant_name = "3scale"
+    prefix = f"{service_name}-{tenant_name}"
+    proxy = service.proxy.list()
+    superdomain = configuration.superdomain
+
+    assert proxy["endpoint"] == f"https://{prefix}-apicast-production.{superdomain}:443"
+
+    assert proxy["sandbox_endpoint"] == f"https://{prefix}-apicast-staging.{superdomain}:443"
