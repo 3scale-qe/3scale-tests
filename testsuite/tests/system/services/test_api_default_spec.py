1915ca9e2b9d078ed21ed7ec17924553e8da97fa
Hchrast <hchrast@redhat.com>
2020-06-11 12:22:10 +0200

    test_api_default_spec

---
+++ testsuite/tests/system/services/test_api_default_spec.py
@@ -0,0 +1,23 @@
+"""
+Rewrite spec/functional_specs/api_default_spec.rb
+"""
+import pytest
+from testsuite.gateways.gateways import Capability
+
+
+def test_staging(api_client):
+    """
+    Test request has to pass and return HTTP 200 for staging client.
+    """
+    response = api_client.get('/get')
+    assert response.status_code == 200
+
+
+@pytest.mark.disruptive
+@pytest.mark.required_capabilities(Capability.PRODUCTION_GATEWAY)
+def test_production(prod_client):
+    """
+    Test request has to pass and return HTTP 200 for prod. client.
+    """
+    response = prod_client().get('/get')
+    assert response.status_code == 200
