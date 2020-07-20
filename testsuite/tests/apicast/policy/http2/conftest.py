2c449e6ac2db7bbfee874724066570b08d6ac029
Jakub Smadiš <jsmadis@redhat.com>
2020-07-20 14:42:34 +0200

    Added test for HTTP2 policy

---
+++ testsuite/tests/apicast/policy/http2/conftest.py
@@ -0,0 +1,24 @@
+"""Fixtures for http2 related tests."""
+import pytest
+
+from testsuite.utils import blame
+
+from testsuite.gateways import TLSApicast, TLSApicastOptions
+
+
+@pytest.fixture(scope="module")
+def staging_gateway(request, configuration):
+    """Deploy tls apicast gateway."""
+    settings_block = {
+        "deployments": {
+            "staging": blame(request, "http2-apicast"),
+            "production": blame(request, "http2-apicast"),
+        }
+    }
+    options = TLSApicastOptions(staging=True, settings_block=settings_block, configuration=configuration)
+    gateway = TLSApicast(requirements=options)
+    gateway.create()
+
+    request.addfinalizer(gateway.destroy)
+
+    return gateway
