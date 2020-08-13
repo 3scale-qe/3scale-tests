8def3841c87ce4ba764dc97090ddd85de09f0ffb
Jakub Smadiš <jsmadis@redhat.com>
2020-08-13 11:23:14 +0200

    Test Batcher policy with mapping rules, regression test for THREESCALE-5513

---
+++ testsuite/tests/apicast/policy/batcher/test_batcher_policy_mapping_rules.py
@@ -0,0 +1,70 @@
+"""
+    Test checks that mapping rules are working with batcher policy
+    Regression test for https://issues.redhat.com/browse/THREESCALE-5513
+"""
+
+from time import sleep
+import pytest
+import requests
+
+from packaging.version import Version  # noqa # pylint: disable=unused-import
+from testsuite import TESTED_VERSION, rawobj  # noqa # pylint: disable=unused-import
+
+
+pytestmark = pytest.mark.skipif("TESTED_VERSION < Version('2.9')")
+
+
+@pytest.fixture(scope="module")
+def policy_settings():
+    """Set policy settings"""
+    return rawobj.PolicyConfig("3scale_batcher", {"batch_report_seconds": 50})
+
+
+@pytest.fixture(scope="module")
+def service(service):
+    """
+        Add the mapping rules
+    """
+    proxy = service.proxy.list()
+
+    metric = service.metrics.list()[0]
+
+    # delete implicit '/' rule
+    proxy.mapping_rules.delete(proxy.mapping_rules.list()[0]["id"])
+
+    proxy.mapping_rules.create(rawobj.Mapping(metric, pattern='/get'))
+
+    proxy.update()
+
+    return service
+
+
+@pytest.fixture(scope='module')
+def api_client(application):
+    """We are testing path that doesn't match mapping rule so we need to disable retry"""
+    assert application.api_client().get("/get").status_code == 200
+
+    session = requests.Session()
+    session.auth = application.authobj
+    return application.api_client(session=session)
+
+
+def test_batcher_policy_append(api_client, application):
+    """Test if return correct number of usages of a service in batch"""
+    analytics = application.threescale_client.analytics
+    usage_before = analytics.list_by_service(application["service_id"], metric_name="hits")["total"]
+
+    for i in range(3):
+        response = api_client.get("/get")
+        assert response.status_code == 200, f"{i}. iteration was unsuccessful"
+
+        response = api_client.get("/anything/no_mapping_rule_match")
+        assert response.status_code == 404, f"{i}. iteration was unsuccessful"
+
+    usage_after = analytics.list_by_service(application["service_id"], metric_name="hits")["total"]
+    assert usage_after == usage_before
+
+    sleep(50)
+
+    usage_after = analytics.list_by_service(application["service_id"], metric_name="hits")["total"]
+    assert usage_after == usage_before + 4
