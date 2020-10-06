bd8eead8a39cda2d6546c7c10385f20565d38edd
Hchrast <hchrast@redhat.com>
2020-10-06 14:58:10 +0200

    test_caching_batching_allow/resilient/strict_mode

---
+++ testsuite/tests/apicast/policy/combination/test_caching_batching_resilient_mode.py
@@ -0,0 +1,76 @@
+"""
+Test caching policy with strict mode.
+"""
+from time import sleep
+import pytest
+from packaging.version import Version  # noqa # pylint: disable=unused-import
+from testsuite import TESTED_VERSION, rawobj  # noqa # pylint: disable=unused-import
+from testsuite.gateways.gateways import Capability
+
+pytestmark = [pytest.mark.skipif("TESTED_VERSION < Version('2.9.1')"),
+              pytest.mark.disruptive,
+              pytest.mark.required_capabilities(Capability.PRODUCTION_GATEWAY),
+              pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-5753")]
+BATCH_REPORT_SECONDS = 50
+
+
+@pytest.fixture(scope="module")
+def service(service):
+    """
+    Adds policies to service RESILIENT mode.
+    Sets the frequency of batch reports that APIcast sends to the 3Scale backend. (Default is 10)
+    """
+    batcher = rawobj.PolicyConfig("3scale_batcher",
+                                  {"batch_report_seconds": BATCH_REPORT_SECONDS, "auths_ttl": BATCH_REPORT_SECONDS})
+    caching = rawobj.PolicyConfig("caching", {"caching_type": "resilient"})
+    service.proxy.list().policies.insert(0, batcher, caching)
+    return service
+
+
+def test_caching_batching_strict_mode(prod_client, openshift, application):
+    """
+    Test caching policy with caching mode set to STRICT
+        - makes request to production gateway with valid credentials
+        - scales backend-listener down to 0
+    tests if:
+        - response with valid credentials have status_code == 200
+        - response with same invalid credentials as before have status_code == 403
+        - response with new valid credentials have status_code == 403
+    - scales back backend-listener up to starting value
+    - if returns correct number of usages of a service in batch after backend was unavailable
+    """
+    openshift = openshift()
+    replicas = openshift.get_replicas("backend-listener")
+    client = prod_client(application)
+
+    response = client.get("/", params=None)
+    assert response.status_code == 200
+
+    openshift.scale("backend-listener", 0)
+
+    analytics = application.threescale_client.analytics
+    usage_before = analytics.list_by_service(application["service_id"], metric_name="hits")["total"]
+
+    try:
+        # Test if response succeed on production calls with valid credentials
+        response = client.get("/", params=None)
+        assert response.status_code == 200
+
+        # Test if response fail on production calls with known invalid credentials
+        response = client.get("/", params={"user_key": ":user_key"})
+        assert response.status_code == 403
+
+        usage_after = analytics.list_by_service(application["service_id"], metric_name="hits")["total"]
+        assert usage_after == usage_before
+
+        # We need to wait for batcher to try to report to the backend-listener
+        # Then the cache is invalidated with last request with invalid credentials
+        sleep(BATCH_REPORT_SECONDS + 1)
+
+        response = client.get("/", params=None)
+        assert response.status_code == 200
+    finally:
+        openshift.scale("backend-listener", replicas)
+
+    usage_after = analytics.list_by_service(application["service_id"], metric_name="hits")["total"]
+    assert usage_after == usage_before
