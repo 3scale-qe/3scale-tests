739698146e65a1e85b6c24f4c75f5c40fe9492c7
Marian Ganisin <mganisin@redhat.com>
2021-01-21 10:03:09 -0500

    get jaeger location from testenv tools

--- testsuite/tests/apicast/parameters/jaeger/conftest.py
+++ testsuite/tests/apicast/parameters/jaeger/conftest.py
@@ -1,6 +1,8 @@
 """
 Conftest for the jaeger tests
 """
+
+from weakget import weakget
 import pytest
 
 from testsuite.utils import blame
@@ -8,12 +10,14 @@ from testsuite.jaeger import Jaeger
 
 
 @pytest.fixture(scope="module")
-def jaeger(testconfig):
+def jaeger(testconfig, tools):
     """
     Returns the Jaeger client class used to communicate with jaeger
     configured based on the values from testconfig
     """
-    return Jaeger(testconfig["fixtures"]["jaeger"]["url"],
+
+    url = weakget(testconfig)["fixtures"]["jaeger"]["url"] % tools["jaeger"]
+    return Jaeger(url,
                   testconfig["fixtures"]["jaeger"]["config"],
                   testconfig["ssl_verify"])
 
