ba30b4ae978351c29a5ab30712123d2bbc2e9c5e
Marian Ganisin <mganisin@redhat.com>
2021-02-11 10:28:49 -0500

    remove netifaces from requirements

--- testsuite/tests/apicast/policy/ip_check/conftest.py
+++ testsuite/tests/apicast/policy/ip_check/conftest.py
@@ -2,10 +2,6 @@
 import pytest
 import requests
 
-from netifaces import interfaces, ifaddresses, AF_INET  # pylint: disable = no-name-in-module
-
-from testsuite.echoed_request import EchoedRequest
-
 
 @pytest.fixture(scope="module")
 def ip4_addresses(private_base_url):
@@ -14,14 +10,7 @@ def ip4_addresses(private_base_url):
     A request is sent to httpbin to determine the real ip addresses
     for container based deplyoment
     """
-    response = requests.get(private_base_url('httpbin') + '/anything',
-                            verify=False)
 
-    echoed_request = EchoedRequest.create(response)
-    ip_list = set(echoed_request.json['origin'].split(','))
-    for interface in interfaces():
-        interface = ifaddresses(interface)
-        if AF_INET in interface:
-            for link in interface[AF_INET]:
-                ip_list.add(link['addr'])
-    return list(ip_list)
+    response = requests.get(private_base_url('httpbin_nossl').rstrip("/") + '/ip')
+
+    return response.json()["origin"].split(",")
