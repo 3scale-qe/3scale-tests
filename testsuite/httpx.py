0bdf9e1b3141e6027b01a41ce6e64fc888918c4a
Marian Ganisin <mganisin@redhat.com>
2021-03-10 14:30:48 -0500

    Progressive retry to HttpxXlient (with slight behavior change)
    Using backoff to implement progressive retry. Also introduce
    UnexpectedResponse exception used also for retry mechanism.
    
    Raise of UnexpectedResponse is change in behavior, however it should be
    desired state and no code should be changed to handle it (unless
    intentionall). UnexpectedResponse will cause error in tests in case of
    unexpected response what might be desired state actually.

--- testsuite/httpx.py
+++ testsuite/httpx.py
@@ -1,16 +1,23 @@
 """Http client with HTTPX library supporting HTTP/1.1 and HTTP/2"""
 import functools
-import time
 from typing import Iterable, Generator
 
 from httpx import Client, Request, Response, URL, Auth, create_ssl_context
 from threescale_api.resources import Application, Service
+import backoff
 
 from testsuite.lifecycle_hook import LifecycleHook
 
 
 # pylint: disable=too-few-public-methods
 from testsuite.utils import basic_auth_string
+
+
+class UnexpectedResponse(Exception):
+    """Slightly different response attributes were expected"""
+    def __init__(self, msg, response):
+        super().__init__(msg)
+        self.response = response
 
 
 class HttpxHook(LifecycleHook):
@@ -65,6 +72,7 @@ class HttpxClient:
         This method is needed for compatibility with HttpClient
         """
 
+    @backoff.on_exception(backoff.fibo, UnexpectedResponse, max_tries=8)
     def request(self, method, path,
                 content=None, data=None, files=None, json=None,
                 params=None, headers=None, cookies=None,
@@ -73,25 +81,21 @@ class HttpxClient:
         auth = auth or self.auth
         self._client.auth = auth
 
-        response = None
-        for _ in range(60):
-            response = self._client.request(
-                method=method,
-                url=path,
-                content=content,
-                data=data,
-                files=files,
-                json=json,
-                params=params,
-                headers=headers,
-                cookies=cookies,
-                allow_redirects=allow_redirects,
-                timeout=timeout)
+        response = self._client.request(
+            method=method,
+            url=path,
+            content=content,
+            data=data,
+            files=files,
+            json=json,
+            params=params,
+            headers=headers,
+            cookies=cookies,
+            allow_redirects=allow_redirects,
+            timeout=timeout)
 
-            if response.status_code not in self._status_forcelist:
-                return response
-            # Sleep 1 second before we try to make the request once again
-            time.sleep(1)
+        if response.status_code in self._status_forcelist:
+            raise UnexpectedResponse(f"Didn't expect '{response.status_code}' status code", response)
 
         return response
 
