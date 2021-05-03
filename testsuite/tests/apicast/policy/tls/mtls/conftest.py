ba17c2053317414df48b93382a0fcfe4cc82e8e0
David Rajnoha <drajnoha@redhat.com>
2021-05-03 22:56:20 +0200

    Added tests testing the reuse of tls connection when APIcast communicates with backends using TLS passthrough routes

--- testsuite/tests/apicast/policy/tls/mtls/conftest.py
+++ testsuite/tests/apicast/policy/tls/mtls/conftest.py
@@ -32,29 +32,52 @@ def httpbin_certificate(request, authority_and_code, configuration) -> Certifica
 
 
 @pytest.fixture(scope="module")
-def httpbin(staging_gateway, request, httpbin_certificate, authority_and_code):
-    """Deploys httpbin with mTLS enabled"""
-    path = resources.files('testsuite.resources.tls').joinpath('httpbin_go.yaml')
-    authority, _ = authority_and_code
-    name = blame(request, "httpbin-mtls")
-    parameters = {
-        "NAME": name,
-        "CERTIFICATE": httpbin_certificate.certificate,
-        "CERTIFICATE_KEY": httpbin_certificate.key,
-        "CA_CERTIFICATE": authority.certificate,
-    }
-    staging_gateway.openshift.new_app(path, parameters)
-    # pylint: disable=protected-access
-    staging_gateway.openshift._wait_for_deployment(name)
+def httpbin(custom_httpbin, request):
+    """
+    Deploys httpbin with mTLS enabled
+    """
+    return custom_httpbin(blame(request, "httpbin-mtls"))
 
-    def _delete():
-        staging_gateway.openshift.delete_app(name, "all")
 
-    request.addfinalizer(_delete)
-    return f"https://{name}:8443"
+@pytest.fixture(scope="module")
+def custom_httpbin(staging_gateway, request, httpbin_certificate, authority_and_code, openshift):
+    """
+    Deploys httpbin with a custom name with mTLS enabled.
+    If tls_route_type is set, creates routes for the backend with given TLS type
+    and returns the public route of the backend.
+    """
+    openshift = openshift()
+
+    def _httpbin(name, tls_route_type=None):
+        path = resources.files('testsuite.resources.tls').joinpath('httpbin_go.yaml')
+        authority, _ = authority_and_code
+        name = blame(request, name)
+        parameters = {
+            "NAME": name,
+            "CERTIFICATE": httpbin_certificate.certificate,
+            "CERTIFICATE_KEY": httpbin_certificate.key,
+            "CA_CERTIFICATE": authority.certificate,
+        }
+
+        def _delete():
+            staging_gateway.openshift.delete_app(name, "all")
+        request.addfinalizer(_delete)
+
+        staging_gateway.openshift.new_app(path, parameters)
+        # pylint: disable=protected-access
+        staging_gateway.openshift._wait_for_deployment(name)
+
+        if tls_route_type is not None:
+            openshift.routes.create(name, tls_route_type, service=name)
+            routes = openshift.routes[name]
+            return f"https://{routes['spec']['host']}:443"
+
+        return f"https://{name}:8443"
+
+    return _httpbin
 
 
 @pytest.fixture(scope="module")
 def service_proxy_settings(httpbin):
-    "Dict of proxy settings to be used when service created"
+    """Dict of proxy settings to be used when service created"""
     return rawobj.Proxy(httpbin)
