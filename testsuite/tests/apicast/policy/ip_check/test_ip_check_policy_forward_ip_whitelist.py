c3d8038bf86de1243ff8259a648f6bd5ba3d064e
David Rajnoha <drajnoha@redhat.com>
2021-05-13 15:31:41 +0200

    add test testing the ip starting with ',' and fixing the test_ip_check_policy_forward_ip_whitelist.py

--- testsuite/tests/apicast/policy/ip_check/test_ip_check_policy_forward_ip_whitelist.py
+++ testsuite/tests/apicast/policy/ip_check/test_ip_check_policy_forward_ip_whitelist.py
@@ -2,7 +2,6 @@
 Rewrite spec/functional_specs/policies/ip_check/ip_check_forward_whitelist_spec.rb
 """
 import pytest
-
 from testsuite import rawobj
 
 
@@ -10,9 +9,9 @@ from testsuite import rawobj
 def policy_settings(ip4_addresses):
     """Update policy settings"""
     return rawobj.PolicyConfig("ip_check", {
-        "check-type": "whitelist",
+        "check_type": "whitelist",
         "client_ip_sources": ["X-Forwarded-For"],
-        "ips_list": ip4_addresses
+        "ips": ip4_addresses
     })
 
 
@@ -22,11 +21,21 @@ def test_ip_check_policy_ip_blacklisted(api_client):
     assert response.status_code == 200
 
 
-def test_ips_with_random_port(api_client, ip4_addresses):
+@pytest.mark.parametrize("ip_addresses,status_code",
+                         [(None, 200),
+                          (["10.10.10.10"], 403),
+                          pytest.param([",10.10.10.10"], 403,
+                                       marks=[pytest.mark.xfail,
+                                       pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-7075")])])
+def test_ips_with_random_port(ip_addresses, status_code, api_client, ip4_addresses):
     """
     Test must pass all the ip4 addresses with port because that are white listed.
+    Test must reject the ip4 addresses that are not whitelisted.
+    Test must reject the invalid ip4 addresses starting with comma.
     """
+    ip_addresses = ip_addresses or ip4_addresses
+
     client = api_client()
-    for ip_address in ip4_addresses:
+    for ip_address in ip_addresses:
         response = client.get('/get', headers={'X-Forwarded-For': f"{ip_address}:12345"})
-        assert response.status_code == 200, f"failed on ip: {ip_address}"
+        assert response.status_code == status_code, f"failed on ip: {ip_address}"
