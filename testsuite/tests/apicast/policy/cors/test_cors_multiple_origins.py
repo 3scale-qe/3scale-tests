8667d97cf8d0e855b951bc26e3795e4ffdf1f51c
David Rajnoha <drajnoha@redhat.com>
2021-05-27 14:37:29 +0200

    Updated the cors policy test so the settings of the policy will be correct

--- testsuite/tests/apicast/policy/cors/test_cors_multiple_origins.py
+++ testsuite/tests/apicast/policy/cors/test_cors_multiple_origins.py
@@ -2,9 +2,7 @@
 Tests the cors policy configured to allow multiple origins based on a regex value.
 When the "origin" header matches the regex, the "Access-Control-Allow-Origin" header
 is set to the value of the "origin" header.
-If not, the "Access-Control-Allow-Origin" is not set. This case is not tested here, as all the
-backends we are using are setting the "Access-Control-Allow-Origin", and the case is covered
-by a unit test.
+If not, the "Access-Control-Allow-Origin" is not set.
 """
 
 import pytest
@@ -20,15 +18,21 @@ pytestmark = [
 
 
 @pytest.fixture(scope="module")
-def policy_settings():
+def service(service):
     """
-    Configure the cors policy to set the "Access-Control-Allow-Origin" based on the given regex.
+    Set the cors policy to allow requests with "localhost" origin.
+    Also sets the headers policy to remove the "Access-Control-Allow-Origin" header from
+    the upstream API, so we can test that this header is added by the cors policy.
     """
-    return rawobj.PolicyConfig("cors", {
+    proxy = service.proxy.list()
+    proxy.policies.insert(0, rawobj.PolicyConfig("cors", {
         "allow_methods": ["GET", "POST"],
         "allow_credentials": True,
         "allow_origin": "(foo|bar).example.com"
-        })
+        }))
+    proxy.policies.insert(0, rawobj.PolicyConfig("headers", {
+        "response": [{"op": "delete", "header": "Access-Control-Allow-Origin"}]}))
+    return service
 
 
 def test_cors_headers_for_same_origin(api_client):
@@ -39,3 +43,12 @@ def test_cors_headers_for_same_origin(api_client):
     """
     response = api_client().get("/get", headers=dict(origin="foo.example.com"))
     assert response.headers.get("Access-Control-Allow-Origin") == "foo.example.com"
+
+
+def test_cors_headers_for_disallowed_origin(api_client):
+    """
+    Sends a url in "origin" header not matching the regex.
+    Asserts that the "Access-Control-Allow-Origin" response header is not set by the policy
+    """
+    response = api_client().get("/get", headers=dict(origin="disallowed.example.com"))
+    assert "Access-Control-Allow-Origin" not in response.headers
