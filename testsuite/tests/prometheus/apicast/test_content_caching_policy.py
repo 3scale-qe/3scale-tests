196ab1f3cfcff99b5e039245944af20b099f3861
David Rajnoha <drajnoha@redhat.com>
2021-05-27 14:14:26 +0200

    fixed the content caching test

--- testsuite/tests/prometheus/apicast/test_content_caching_policy.py
+++ testsuite/tests/prometheus/apicast/test_content_caching_policy.py
@@ -1,6 +1,8 @@
 """
 Test Prometheus metric for content_caching.
 """
+import time
+
 import pytest
 
 from packaging.version import Version  # noqa # pylint: disable=unused-import
@@ -51,13 +53,15 @@ def api_client(api_client):
 
 @pytest.mark.disruptive
 @pytest.mark.parametrize(("client", "apicast"), [("api_client", "3scale Apicast Staging"),
-                                                 ("prod_client", "3scale Apicast Production")],
+                                                 ("prod_client", "3scale Apicast Production")
+                                                 ],
                          ids=["Staging Apicast", "Production Apicast"])
 def test_content_caching(request, prometheus, client, apicast):
     """
     Test if cache works correctly and if prometheus contains content_caching metric.
     """
     client = request.getfixturevalue(client)
+    counts_before = extract_caching(prometheus, "content_caching", apicast)
 
     response = client.get("/anything/test", headers=dict(origin="localhost"))
     assert response.status_code == 200
@@ -67,7 +71,29 @@ def test_content_caching(request, prometheus, client, apicast):
     assert response.status_code == 200
     assert response.headers.get("X-Cache-Status") == "HIT"
 
+    # prometheus is downloading this metrics each 5 seconds, we need to wait
+    time.sleep(10)
+
     metrics = prometheus.get_metrics(apicast)
     metrics = [m["metric"] for m in metrics["data"]]
-
     assert "content_caching" in metrics
+
+    counts_after = extract_caching(prometheus, "content_caching", apicast)
+
+    assert int(counts_after['HIT']) == int(counts_before['HIT']) + 1
+
+
+def extract_caching(prometheus, query, apicast):
+    """
+    Given a prometheus query, returns dict with response
+    codes and counts associated with the response code
+    """
+    metric_response_codes = prometheus.get_metric(query)
+    key_value_map = {'EXPIRED': 0, 'HIT': 0, 'MISS': 0}
+    for response_code_metric in metric_response_codes:
+        if apicast != response_code_metric['metric']['job']:
+            continue
+
+        key_value_map[response_code_metric['metric']['status']] \
+            = response_code_metric['value'][1]
+    return key_value_map
