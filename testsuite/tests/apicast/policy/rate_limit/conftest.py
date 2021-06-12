c1b5509cea610d16be95578121ba4ecefd1c5521
Hchrast <hchrast@redhat.com>
2021-06-12 20:06:35 +0200

    fixed flaky test

--- testsuite/tests/apicast/policy/rate_limit/conftest.py
+++ testsuite/tests/apicast/policy/rate_limit/conftest.py
@@ -1,8 +1,10 @@
 """
 Default conftest for rate limit tests
 """
+import warnings
 import pytest
 import pytest_cases
+import threescale_api.errors
 
 from testsuite import rawobj
 from testsuite.utils import blame
@@ -39,13 +41,51 @@ def application(service_plus, custom_app_plan, custom_application, request):
     return custom_application(rawobj.Application(blame(request, "app"), plan))
 
 
+@pytest.fixture
+def prod_client(production_gateway, application, request):
+    """
+    Duplicate with different scope, the reasoning behind duplicate is that if called prod_client from root conftest
+    it didn't behave as expected.
+    Prepares application and service for production use and creates new production client
+
+    Parameters:
+        app (Application): Application for which create the client.
+        promote (bool): If true, then this method also promotes proxy configuration to production.
+        version (int): Proxy configuration version of service to promote.
+        redeploy (bool): If true, then the production gateway will be reloaded
+
+    Returns:
+        api_client (HttpClient): Api client for application
+
+    """
+
+    def _prod_client(app=application, promote: bool = True, version: int = -1, redeploy: bool = True):
+        if promote:
+            if version == -1:
+                version = app.service.proxy.list().configs.latest()['version']
+            try:
+                app.service.proxy.list().promote(version=version)
+            except threescale_api.errors.ApiClientError:
+                warnings.warn(str(threescale_api.errors.ApiClientError))
+
+        if redeploy:
+            production_gateway.reload()
+
+        client = app.api_client(endpoint="endpoint")
+        request.addfinalizer(client.close)
+        return client
+
+    return _prod_client
+
+
 @pytest_cases.fixture
-def client(application):
-    """function-scoped api_client
+def client(application, prod_client):
+    """
+    Production client for first application.
+    Furthermore urllib3 has to be boosted to allow enough connections
+    """
 
-    Furthermore urllib3 has to be boosted to allow enough connections"""
-
-    client = application.api_client()
+    client = prod_client(application)
 
     client.extend_connection_pool(500)
     yield client
@@ -53,10 +93,11 @@ def client(application):
 
 
 @pytest_cases.fixture
-def client2(application2):
+def client2(application2, prod_client):
     """
-    Sets ssl_verify for api client
+    Production client for second application.
     """
-    client = application2.api_client()
+
+    client = prod_client(application2)
     yield client
     client.close()
