41e3d65276d94edda34892fb3db532545eab7cb8
Jakub Smolar <jsmolar@redhat.com>
2021-07-28 18:59:54 +0200

    Fixed xpath for submit btn to accept input and button

--- testsuite/ui/widgets/buttons.py
+++ testsuite/ui/widgets/buttons.py
@@ -40,5 +40,5 @@ class ThreescaleEditButton(GenericLocatorWidget):
 class ThreescaleSubmitButton(Button):
     """Specific Submit button of 3scale pages"""
 
-    def __init__(self, parent=None, locator=".//button[@type='submit']", logger=None):
+    def __init__(self, parent=None, locator="//*[(self::input or self::button) and @type='submit']", logger=None):
         super().__init__(parent, locator=locator, logger=logger)
