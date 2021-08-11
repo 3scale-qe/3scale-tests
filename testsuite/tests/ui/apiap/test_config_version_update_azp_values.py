3362840dd25e1568d6bfb4d2bc91aaa5320e158b
Hchrast <hchrast@redhat.com>
2021-08-11 11:42:36 +0200

    add test for updated config version

---
+++ testsuite/tests/ui/apiap/test_config_version_update_azp_values.py
@@ -0,0 +1,46 @@
+"""
+"https://issues.redhat.com/browse/THREESCALE-6468"
+"""
+
+import pytest
+
+from testsuite.ui.views.admin.product.integration.configuration import ProductConfigurationView
+from testsuite.ui.views.admin.product.integration.settings import ProductSettingsView
+
+
+# pylint: disable=unused-argument
+@pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-6468")
+def test_config_version(login, service, navigator, threescale,  browser, ):
+    """
+    Test:
+        - Navigates to Product Settings view
+        - Changes authentication to OpenID, updates Product
+        - Navigates to Product Configuration
+        - Assert that there is notification for outdated config
+        - Assert that promote button is enabled
+        - Promotes to staging
+        - Navigates to Product Settings view
+        - Changes default ClientID value "azp", updates Product
+        - Assert that the desired value saved to configuration
+        - Navigates to Product Configuration
+        - Assert that the promote button is disabled
+        - Assert that there isn't notification for outdated config
+    """
+    settings = navigator.navigate(ProductSettingsView, product=service)
+    settings.change_authentication('service_proxy_authentication_method_oidc')
+    settings = navigator.navigate(ProductConfigurationView, product=service)
+
+    assert settings.outdated_config.is_enabled
+    assert settings.configuration.staging_promote_btn.is_displayed
+    settings.configuration.staging_promote_btn.click()
+
+    settings = navigator.navigate(ProductSettingsView, product=service)
+    settings.update_client_id('azpza')
+    settings.update_button.click()
+    assert settings.client_id.value == 'azpza'
+
+    settings = navigator.navigate(ProductConfigurationView, product=service)
+    assert settings.configuration.staging_promote_btn.is_enabled
+    assert settings.outdated_config.is_displayed
+
+    assert service.proxy.list().configs.latest().entity['content']['proxy']['authentication_method']
