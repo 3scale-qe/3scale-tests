6bc60610f300fa110b3e916ab02ab39155e49dbb
Jakub Urban <jaurban@redhat.com>
2021-09-08 12:10:30 +0200

    Add tests for account search

--- testsuite/tests/ui/test_account_search.py
+++ testsuite/tests/ui/test_account_search.py
@@ -46,3 +46,51 @@ def test_search_account(login, navigator, custom_ui_account, ui_application, req
         accounts.search(key)
         assert accounts.table.row()[1].text == org_name
         assert accounts.table.row().state.text == "Approved"
+
+
+# pylint: disable=unused-argument
+def test_search_multiple_accounts(login, navigator, custom_account, request):
+    """
+    Preparation:
+        - Create 4 custom account
+    Test if:
+        - you search account by base name it will return the correct ones (first 3)
+        - you search account by specific name it will return the correct one
+    """
+    name = blame(request, "")
+    params = [dict(org_name=f"{name}_name", username="username", email="email@anything.invalid", password="123456"),
+              dict(org_name=f"{name}_name2", username="username2", email="email2@anything.invalid", password="123456"),
+              dict(org_name=f"{name}", username="user", email="mail@anything.valid", password="123456"),
+              dict(org_name=blame(request, "RedHat"), username="random", email="random@random.random",
+                   password="123456")
+              ]
+    for param in params:
+        custom_account(param)
+
+    accounts = navigator.navigate(AccountsView)
+    accounts.search(name)
+
+    rows = accounts.table.rows()
+    assert len(list(rows)) == 3
+
+    accounts.search(f"{name}_name2")
+    assert accounts.table.row()[1].text == f"{name}_name2"
+    assert accounts.table.row().state.text == "Approved"
+
+
+def test_search_non_existing_value(request, login, navigator, custom_account):
+    """
+    Preparation:
+        - Create custom account
+    Test if:
+        - you search account by non-existing-value it won't return anything
+    """
+    username = blame(request, "username")
+    params = dict(org_name="org_name", username=username, email=f"{blame(request, 'email')}@anything.invalid",
+                  password="123456")
+    custom_account(params)
+
+    accounts = navigator.navigate(AccountsView)
+    accounts.search("non-existing-value")
+
+    assert next(accounts.table.rows())[0].text == "No results."
