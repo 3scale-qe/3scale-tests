3bee8d187bdde63f7e5da1b0d6a06a937a5516b2
jsmolar <jsmolar@redhat.com>
2021-09-29 13:46:07 +0200

    Prepared ApiDocs view for addittional tests

---
+++ testsuite/tests/ui/api_docs/test_autocomplete.py
@@ -0,0 +1,29 @@
+"""Rewrite of spec/ui_specs/autocomplete_spec.rb"""
+from testsuite import rawobj
+from testsuite.ui.views.admin.settings.api_docs import APIDocsView
+
+
+# pylint: disable=unused-argument
+def test_autocomplete(login, navigator, service, testconfig):
+    """
+        A test that checks whether autocomplete works as expected on API Docs
+
+        - creates a service (product) that will be used in auto-complete
+        - goes to API Docs and searches for "Service Read" category
+        - fills out "Service Read" arguments and then sends it to server
+        - checks whether all went as expected
+    """
+    token = testconfig["threescale"]["admin"]["token"]
+
+    api_docs_page = navigator.navigate(APIDocsView)
+    endpoint = api_docs_page.endpoint("Service Read")
+    endpoint.fill(rawobj.ApiDocParams(token))
+    endpoint.get_param("service_ids").click()
+
+    product_id_element = api_docs_page.get_id_input_by_name(service["name"])
+    auto_comp_prod_id = product_id_element.text
+    product_id_element.click()
+    assert auto_comp_prod_id == str(service["id"])
+
+    endpoint.submit_button.click()
+    assert endpoint.status_code() == "200"
--- testsuite/ui/views/admin/settings/api_docs.py
+++ testsuite/ui/views/admin/settings/api_docs.py
@@ -1,27 +1,84 @@
 """
 View representations of 3scale API Docs pages
 """
+
+import backoff
 from widgetastic.exceptions import NoSuchElementException
-from widgetastic.widget import Text, ParametrizedView, ParametrizedLocator
+from widgetastic.widget import Text, ParametrizedView, ParametrizedLocator, View, TextInput
 from widgetastic_patternfly4 import Button
 
 from testsuite.ui.views.admin.settings import BaseSettingsView
 
 
+class _Endpoint(ParametrizedView):
+    """
+    Parametrized View that represents a section (for example "Service Read") in API Docs
+    """
+    PARAMETERS = ('endpoint_name',)
+    ROOT = ParametrizedLocator('//a[text()={endpoint_name|quote}]/ancestor::li[contains(@class, "operation")]')
+    ALL_ENDPOINTS = './/a[@class="toggle"]'
+    name = Text(locator='.//a[@class="toggle"]')
+    access_token = TextInput(name='access_token')
+    submit_button = Button(locator='.//button[@class="submit"]')
+    status_code_field = Text(locator='.//div[@class="block response_code"]/pre')
+
+    def unroll(self):
+        """Unrolls the endpoint section"""
+        if not self.submit_button.is_displayed:
+            self.name.click()
+            self.submit_button.wait_displayed()
+
+    def status_code(self):
+        """Returns request status code"""
+        self.status_code_field.wait_displayed()
+        return self.status_code_field.read()
+
+    def get_param(self, param):
+        """Returns input param field"""
+        return self.browser.element(f'.//input[contains(@data-threescale-name, "{param}")]')
+
+    def fill(self, values: dict):
+        """
+        Widgetastic fill method override. Fills Parameter values for API doc endpoint
+        Args:
+            :param values: dict of param_name:param_value
+        """
+        self.unroll()
+        for param in values:
+            self.get_param(param).send_keys(values.get(param))
+
+    def send_request(self, params: dict):
+        """
+        Wraps basic use case for API docs endpoint. Fills endpoint params, sends request and returns response code
+        Args:
+            :param params: dict of param_name:param_value
+            :return response code
+        """
+        self.fill(params)
+        self.submit_button.click()
+        return self.status_code_field.read()
+
+    @classmethod
+    def all(cls, browser):
+        """Override of parent class `all` method"""
+        return [(browser.text(el),) for el in browser.elements(cls.ALL_ENDPOINTS)]
+
+
 class APIDocsView(BaseSettingsView):
     """
-        View representation of API Docs page
+    View representation of API Docs page
     """
     path_pattern = '/p/admin/api_docs'
     page_title = Text(locator='//*[@id="content"]/h1')
     service_management_api_category = Text(locator='//*[@data-name="service_management_api"]')
     account_management_api_category = Text(locator='//*[@data-name="account_management_api"]')
     policy_registry_api_category = Text(locator='//*[@data-name="policy_registry_api"]')
+    endpoint = View.nested(_Endpoint)
 
     def get_id_input_by_name(self, name):
         """
-            Helper method for autocomplete part of a test that tests "Service Read"
-            (it finds the correct autocorrect text and returns its ID)
+        Helper method for autocomplete part of a test that tests "Service Read"
+        (it finds the correct autocorrect text and returns its ID)
         """
         auto_complete_part = '//*[@id="content"]/div/div[13]/ul/li'
         for li_index in range(len(self.browser.elements(auto_complete_part))):
@@ -29,42 +86,16 @@ class APIDocsView(BaseSettingsView):
                 return self.browser.element(auto_complete_part + f'[{li_index + 1}]/span')
         raise NoSuchElementException('No element was found')
 
-    # pylint: disable=abstract-method, disable=invalid-name
-    class endpoint_section(ParametrizedView):
-        """
-            Parametrized View that represents a section (for example "Service Read") in API Docs
-
-            //*[@id="account_management_api_endpoint_operations"]/li[177]/div[2]
-        """
-        PARAMETERS = ('endpoint_name',)
-        ROOT = ParametrizedLocator('//a[text()={endpoint_name|quote}]/ancestor::li[@class="get operation"]')
-        name = Text(locator='.//a[@class="toggle"]')
-        submit_button = Button(locator='.//button[@class="submit"]')
-        status_code = Text(locator='.//div[@class="block response_code"]/pre')
-
-        def unroll(self):
-            """
-                unrolls the section, so we can start working with the section
-            """
-            if not self.submit_button.is_displayed:
-                self.name.click()
-                self.submit_button.wait_displayed()
-
-        def get_input_field(self, key):
-            """
-                returns input field for a form part that was provided (for example, if we pass it "access_token",
-                we will receive the input field, where we can type the access token).
-            """
-            return self.browser.element(f'.//input[contains(@data-threescale-name, "{key}")]')
-
-        def fill_input(self, key, value):
-            """
-                gets the required input field and fills it up
-            """
-            self.get_input_field(key).send_keys(value)
-
     def prerequisite(self):
         return BaseSettingsView
+
+    @backoff.on_predicate(backoff.fibo, lambda x: x < 200, max_tries=8)
+    def post_navigate(self, **kwargs):
+        """
+        APIDocsView loads Endpoints dynamically with js script.
+        Once navigated to this View, Navigator waits for all API endpoint sections to be loaded.
+        """
+        return len(self.endpoint)
 
     @property
     def is_displayed(self):
