90a8fa6840b68c6219a0be9067abb04aedbf56d3
Marian Ganisin <mganisin@redhat.com>
2021-10-06 03:14:25 -0400

    Let's try email testing with retry :)
    Use backoff in "usual" formula used elsewhere in the testsuite. This
    doesn't fix the test, it standardizes the retry mechanism and also
    prolongs it little bit.

--- testsuite/tests/system/messages/test_account_message.py
+++ testsuite/tests/system/messages/test_account_message.py
@@ -2,7 +2,15 @@
 Rewrite /spec/functional_specs/account_message_spec.rb
 When sending message to a developer account, the email is send
 """
-import time
+
+import backoff
+
+
+@backoff.on_exception(backoff.fibo, AssertionError, 8, jitter=None)
+def assert_message_received(mailhog, text):
+    """Resilient test on presence of expected message with retry"""
+    messages = mailhog.messages()
+    assert any(m for m in messages["items"] if text in m["Content"]["Body"])
 
 
 def test_account_will_receive_email(mailhog_client, threescale, account):
@@ -17,21 +25,4 @@ def test_account_will_receive_email(mailhog_client, threescale, account):
     threescale.threescale_client.accounts\
         .send_message(entity_id=account.entity_id, body=test_message_body)
 
-    messages = mailhog_client.messages()
-
-    contains_test_message = False
-    retries = 0
-    # if the email is not send within 60 seconds, fails
-    while retries < 6:
-        for email in messages["items"]:
-            if test_message_body in email["Content"]["Body"]:
-                contains_test_message = True
-        if contains_test_message:
-            break
-
-        messages = mailhog_client.messages()
-        time.sleep(10)
-        retries += 1
-
-    assert contains_test_message, f'The email does not not contain the ' \
-                                  f'\'{test_message_body}\' in the body'
+    assert_message_received(mailhog_client, test_message_body)
