52fb6513bf518a89cb12fa28c91e6870a90447b4
Marian Ganisin <mganisin@redhat.com>
2021-12-02 03:03:18 -0500

    Revert backoff on custom_account fixture
    The backoff was supposed to mitigate 409 error returned right after the
    deployment or custom tenant creation.
    
    This didn't fix it as the API returned 422 on repeated calls and made
    the situation even worse under some circumstances. The only efect was
    that the faliure was significantly delayed and test execution prolonged.

--- testsuite/tests/conftest.py
+++ testsuite/tests/conftest.py
@@ -315,7 +315,6 @@ def custom_account(threescale, request, testconfig):
     Args:
         :param params: dict for remote call, rawobj.Account should be used
     """
-    @backoff.on_exception(backoff.fibo, errors.ApiClientError, 8, jitter=None)
     def _custom_account(params, autoclean=True, threescale_client=threescale):
         acc = threescale_client.accounts.create(params=params)
         if autoclean and not testconfig["skip_cleanup"]:
