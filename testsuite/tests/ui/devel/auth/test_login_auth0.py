220ff5203a7b4371301d228e10d2282ae88d2a6f
Jakub Urban <jaurban@redhat.com>
2021-12-06 11:38:55 +0100

    Repair of auth tests

--- testsuite/tests/ui/devel/auth/test_login_auth0.py
+++ testsuite/tests/ui/devel/auth/test_login_auth0.py
@@ -52,7 +52,7 @@ def test_devel_login_auth0(custom_devel_auth0_login, navigator, auth0_user, auth
     """
     custom_devel_auth0_login(auth0_user["email"], auth0_user_password)
     signup_view = SignUpView(navigator.browser)
-    assert signup_view.wait_displayed
+    assert signup_view.wait_displayed()
 
     signup_view.signup("RedHat")
 
