922ccfe2b09c487acf24082d561d7e710bcc1f0b
Jakub Urban <jaurban@redhat.com>
2022-03-14 15:11:43 +0100

    Add tests for Public Base URLs as localhost

---
+++ testsuite/tests/ui/apiap/test_public_base_url.py
@@ -0,0 +1,30 @@
+"""Test for Public Base URLs as localhost"""
+import pytest
+from packaging.version import Version  # noqa # pylint: disable=unused-import
+from widgetastic.widget import Text
+
+from testsuite import TESTED_VERSION  # noqa # pylint: disable=unused-import
+from testsuite.ui.views.admin.product.integration.settings import ProductSettingsView
+
+pytestmark = [
+    pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-7149"),
+    pytest.mark.skipif("TESTED_VERSION < Version('2.12')")
+]
+
+
+# pylint: disable=unused-argument
+def test_public_base_url(login, navigator, service, browser):
+    """
+    Test:
+        - navigate to Product settings page
+        - try to set staging and production Public Base URLs to localhost
+        - assert that it's not possible
+    """
+    settings = navigator.navigate(ProductSettingsView, product=service)
+    settings.update_gateway(staging='https://localhost:80', production='https://localhost:80')
+
+    staging = Text(browser, '//*[@id="service_proxy_attributes_sandbox_endpoint_input"]/p[2]')
+    production = Text(browser, '//*[@id="service_proxy_attributes_endpoint_input"]/p[2]')
+
+    assert staging.text == "can't be localhost"
+    assert production.text == "can't be localhost"
