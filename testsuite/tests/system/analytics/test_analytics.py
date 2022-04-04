4d9381f79dac54a4ba606911e62f7f91e89bf5c5
Marian Ganisin <mganisin@redhat.com>
2022-04-04 10:20:35 +0000

    reliable way to get analytics

--- testsuite/tests/system/analytics/test_analytics.py
+++ testsuite/tests/system/analytics/test_analytics.py
@@ -9,6 +9,7 @@ the number of requests send by the application.
 """
 import pytest
 from testsuite import rawobj
+from testsuite import resilient
 from testsuite.utils import blame
 
 pytestmark = pytest.mark.required_capabilities()
@@ -44,8 +45,9 @@ def test_hits_service(application, api_client, app2):
         assert client2.get("/get").status_code == 200
 
     analytics = app2.threescale_client.analytics
-    metrics_service = analytics.list_by_service(app2["service_id"],
-                                                metric_name="hits")["total"]
+
+    metrics_service = resilient.stats_service_usage(
+        app2.threescale_client, app2["service_id"], "hits", "total", requests_app+requests_app2)
     assert metrics_service == requests_app + requests_app2
 
     metrics_app = analytics.list_by_application(application, metric_name="hits")["total"]
