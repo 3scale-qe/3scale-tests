f0678715d3ea1a8885f4ef23bb6e979cdc6102ac
Marian Ganisin <mganisin@redhat.com>
2022-07-19 11:58:37 +0000

    New test to cover THREESCALE-8483 and THREESCALE-8484

---
+++ testsuite/tests/apicast/policy/test_liquid.py
@@ -0,0 +1,67 @@
+"""
+Test behavior of liquid
+"""
+
+from packaging.version import Version  # noqa # pylint: disable=unused-import
+
+import pytest
+
+from testsuite import rawobj
+from testsuite import TESTED_VERSION  # noqa # pylint: disable=unused-import
+
+
+pytestmark = [
+    pytest.mark.skipif("TESTED_VERSION <= Version('2.12')"),
+    pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-8483"),
+    pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-8484")
+]
+
+
+@pytest.fixture(scope="module")
+def policy_settings():
+    """configures headers in policy"""
+    return rawobj.PolicyConfig("headers", {
+        "response": [
+            {
+                "op": "set", "header": "X-Liquid-String-Doesnt-Contain", "value_type": "liquid",
+                "value": '{% if "abcdef" contains "wxyz" %}fail{% else %}pass{% endif %}'
+            },
+            {
+                "op": "set", "header": "X-Liquid-String-Contains", "value_type": "liquid",
+                "value": '{% if "abcdef" contains "bcd" %}pass{% else %}fail{% endif %}'
+            },
+            {
+                "op": "set", "header": "X-Liquid-Array-Doesnt-Contain", "value_type": "liquid",
+                "value": '{% assign arr = "abcd afooa efghi" | split: " " %}{% if arr contains "foo" %}fail{% else %}pass{% endif%}'# noqa # pylint: disable=line-too-long
+            },
+            {
+                "op": "set", "header": "X-Liquid-Array-Contains", "value_type": "liquid",
+                "value": '{% assign arr = "abcd foo efghi" | split: " " %}{% if arr contains "foo" %}pass{% else %}fail{% endif%}'  # noqa # pylint: disable=line-too-long
+            }
+        ]})
+
+
+@pytest.fixture(scope="module")
+def response(api_client):
+    """Make one request for all tests"""
+    return api_client().get("/get")
+
+
+def test_liquid_string_doesnt_contain(response):
+    """Test liquid 'contains' condition on string that doesn't match"""
+    assert response.headers.get("X-Liquid-String-Doesnt-Contain", "HEADER IS MISSING") == "pass"
+
+
+def test_liquid_string_contains(response):
+    """Test liquid 'contains' condition on string that matches"""
+    assert response.headers.get("X-Liquid-String-Contains", "HEADER IS MISSING") == "pass"
+
+
+def test_liquid_array_doesnt_contain(response):
+    """Test liquid 'contains' condition on array that doesn't match"""
+    assert response.headers.get("X-Liquid-Array-Doesnt-Contain", "HEADER IS MISSING") == "pass"
+
+
+def test_liquid_array_contains(response):
+    """Test liquid 'contains' condition on array that matches"""
+    assert response.headers.get("X-Liquid-Array-Contains", "HEADER IS MISSING") == "pass"
