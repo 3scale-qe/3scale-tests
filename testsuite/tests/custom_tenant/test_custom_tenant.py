09a52f0e6ffbe18bf3e2d1b78e3e284223028ac8
Marian Ganisin <mganisin@redhat.com>
2022-09-08 21:06:49 +0000

    Few new object in test_custom_tenant
    To ensure that account is created properly and in correct tenant account
    has to be created with correct scope, otherwise the objects are created
    at the beginning of the session thus in different tenant.

--- testsuite/tests/custom_tenant/test_custom_tenant.py
+++ testsuite/tests/custom_tenant/test_custom_tenant.py
@@ -4,6 +4,9 @@ Creates a new tenant and tests if the tenant works
 """
 import pytest
 
+from testsuite import rawobj, resilient
+from testsuite.utils import blame
+
 
 @pytest.fixture(scope="session")
 def threescale(custom_tenant, testconfig):
@@ -18,9 +21,38 @@ def threescale(custom_tenant, testconfig):
     return tenant.admin_api(ssl_verify=testconfig["ssl_verify"], wait=0)
 
 
+@pytest.fixture(scope="module")
+def account(custom_account, request, account_password):
+    """Local module scoped account to utilize custom tenant"""
+    iname = blame(request, "id")
+    account = rawobj.Account(org_name=iname, monthly_billing_enabled=None, monthly_charging_enabled=None)
+    account.update(dict(
+        name=iname, username=iname,
+        email=f"{iname}@example.com",
+        password=account_password))
+    account = custom_account(params=account)
+
+    return account
+
+
+@pytest.fixture(scope="module")
+def custom_account(threescale, request, testconfig):
+    """ Local module scoped custom_account to utilize custom tenant
+
+    Args:
+        :param params: dict for remote call, rawobj.Account should be used
+    """
+    def _custom_account(params, autoclean=True, threescale_client=threescale):
+        acc = resilient.accounts_create(threescale_client, params=params)
+        if autoclean and not testconfig["skip_cleanup"]:
+            request.addfinalizer(acc.delete)
+        return acc
+
+    return _custom_account
+
+
 # FIXME: threescale_api.errors.ApiClientError: Response(422): b'{"errors":{"system_name":["must be shorter."]}}
 # Name of the tenant is too long
-@pytest.mark.flaky
 def test_custom_tenant(api_client):
     """
     Makes a request using the tenant
