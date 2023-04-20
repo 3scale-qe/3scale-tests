281d1cdc806b739f4a658f70762339536010669a
Marian Ganisin <mganisin@redhat.com>
2023-04-20 11:33:55 +0000

    Extend test_uri_too_large
    Add test to cover also long parameter

--- testsuite/tests/apicast/test_uri_too_large.py
+++ testsuite/tests/apicast/test_uri_too_large.py
@@ -24,3 +24,17 @@ def test_uri_too_large(api_client, staging_gateway):
     logs = staging_gateway.get_logs()
     for message in WARN_MESSAGES:
         assert message not in logs
+
+
+@pytest.mark.skipif("TESTED_VERSION < Version('2.13')")
+@pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-7906")
+def test_param_too_large(api_client, staging_gateway):
+    """
+    Test that redundant warn logs are not present in apicast logs after 414 Request-URI Too Large response
+    """
+    client = api_client()
+    response = client.get("/anything/anything", params={"long": 10000 * 'a'})
+    assert response.status_code == 414
+    logs = staging_gateway.get_logs()
+    for message in WARN_MESSAGES:
+        assert message not in logs
