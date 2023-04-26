43ae85a287dd3b2cca7f715b5db1322d90c343cc
Marian Ganisin <mganisin@redhat.com>
2023-04-26 06:36:39 +0000

    Disruptive mark to test_apicast_pagination.py
    The test negatively affects others if ran in parallel. Moving it to
    disruptive for now. It doesn't have to be final solution as it requires
    lot's of time. For now this is the way to ensure that the test is used.

--- testsuite/tests/apicast/parameters/test_apicast_pagination.py
+++ testsuite/tests/apicast/parameters/test_apicast_pagination.py
@@ -21,7 +21,7 @@ async def many_services(request, custom_service, service_proxy_settings, lifecyc
     )
 
 
-@pytest.mark.sandbag
+@pytest.mark.disruptive  # this generates high load on 3scale with impact on other tests
 @pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-8373")
 @pytest.mark.required_capabilities(Capability.STANDARD_GATEWAY, Capability.CUSTOM_ENVIRONMENT)
 @pytest.mark.usefixtures("staging_gateway")
