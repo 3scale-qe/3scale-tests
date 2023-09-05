16f39e9da6d7a01d4d5653a7e230806245d873a1
Jakub Urban <jaurban@redhat.com>
2023-09-05 13:30:22 +0200

    Add test for nginx warn in apicast logs

--- testsuite/tests/apicast/test_apicast_logs.py
+++ testsuite/tests/apicast/test_apicast_logs.py
@@ -6,8 +6,20 @@ import pytest
 
 
 @pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-7942")
-def test_apicast_logs(staging_gateway):
+def test_apicast_logs_tmp_file(staging_gateway):
     """
     Test that the logs don't contain permission denied in a tmp file
     """
     assert not re.search("env: '/tmp/[a-zA-Z0-9]*': Permission denied", staging_gateway.get_logs())
+
+
+@pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-7941")
+def test_apicast_logs_nginx_warn(staging_gateway):
+    """
+    Test that the logs don't contain nginx warn
+    """
+    log = (
+        "nginx: [warn] could not build optimal variables_hash, you should increase either variables_hash_max_size: "
+        "1024 or variables_hash_bucket_size: 64; ignoring variables_hash_bucket_size"
+    )
+    assert log not in staging_gateway.get_logs()
