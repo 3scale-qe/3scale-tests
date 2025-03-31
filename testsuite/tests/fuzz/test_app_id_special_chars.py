aa9534b5d9af8314b19982d496ee78bebffcaa00
Martin Kudlej <mkudlej@redhat.com>
2025-03-31 18:24:08 +0200

    add authentication in headers and change test after fix of some issue

--- testsuite/tests/fuzz/test_app_id_special_chars.py
+++ testsuite/tests/fuzz/test_app_id_special_chars.py
@@ -1,5 +1,6 @@
 """Test special characters in app_id and app_key"""
 
+import random
 import pytest
 from threescale_api.resources import Service
 
@@ -9,6 +10,23 @@ from testsuite.capabilities import Capability
 from testsuite.utils import blame
 
 pytestmark = [pytest.mark.required_capabilities(Capability.PRODUCTION_GATEWAY)]
+
+
+@pytest.fixture(scope="module")
+def private_base_url(tools):
+    """Change api_backend to httpbin for service."""
+
+    def _private_base_url():
+        return tools["httpbin"]
+
+    return _private_base_url
+
+
+@pytest.fixture(scope="module")
+def service_proxy_settings(service_proxy_settings):
+    "auth via url params doesn't work because of url encoding of special characters"
+    service_proxy_settings.update(credentials_location="headers")
+    return service_proxy_settings
 
 
 @pytest.fixture(scope="module")
@@ -26,11 +44,12 @@ def app_plan(service, custom_app_plan, request):
 
 @pytest.fixture(scope="module")
 # pylint: disable=too-many-arguments
-def application(app_id, app_key, service, custom_application, app_plan, lifecycle_hooks, request):
+def application(app_id, app_key, credentials_location, service, custom_application, app_plan, lifecycle_hooks, request):
     "application bound to the account and service existing over whole testing session"
     plan = app_plan
     app_obj = rawobj.Application(blame(request, "app"), plan, app_id=app_id, app_key=app_key)
     app = custom_application(app_obj, hooks=lifecycle_hooks)
+    service.proxy.list().update({"credentials_location": credentials_location})
     service.proxy.deploy()
     return app
 
@@ -44,7 +63,7 @@ def client2(application):
 @pytest.fixture(scope="module")
 def app_id(request):
     """indirect fixture for app_id used for creating and used as reference value in assert"""
-    return request.param
+    return "".join(random.sample(request.param, len(request.param)))
 
 
 @pytest.fixture(scope="module")
@@ -53,49 +72,117 @@ def app_key(request):
     return request.param
 
 
+@pytest.fixture(scope="module")
+def credentials_location(request):
+    """indirect fixture for credentials_location used for creating and used as reference value in assert"""
+    return request.param
+
+
 @pytest.mark.parametrize(
-    ("app_id", "app_key"),
+    ("app_id", "app_key", "credentials_location"),
     [
-        pytest.param("MYID", "keykey1"),
+        # credentials located in the headers
+        pytest.param("MYID", "keykey1", "headers"),
         pytest.param(
             "!#$&'(",
             "keykey2",
+            "headers",
             marks=[
-                pytest.mark.xfail,
-                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-9033"),
                 pytest.mark.fuzz,
             ],
         ),
         pytest.param(
             ")*+,-./:",
             "keykey3",
+            "headers",
             marks=[
-                pytest.mark.xfail,
-                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-9033"),
                 pytest.mark.fuzz,
             ],
         ),
         pytest.param(
             ";=?@",
             "keykey4",
+            "headers",
             marks=[
-                pytest.mark.xfail,
-                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-9033"),
                 pytest.mark.fuzz,
             ],
         ),
         pytest.param(
-            "_~",
+            "_~ID",
             "keykey5",
+            "headers",
             marks=[
-                pytest.mark.xfail,
-                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-9033"),
                 pytest.mark.fuzz,
             ],
         ),
         pytest.param(
             '"%<>[\\]^`{|}',
             "keykey6",
+            "headers",
+            marks=[
+                pytest.mark.fuzz,
+            ],
+        ),
+        pytest.param(
+            "{}*~KEY",
+            "keykey7",
+            "headers",
+            marks=[
+                pytest.mark.fuzz,
+            ],
+        ),
+        pytest.param(
+            "9999_",
+            "keykey8",
+            "headers",
+            marks=[
+                pytest.mark.fuzz,
+            ],
+        ),
+        # credentials located in the query
+        pytest.param("MYID", "keykey1", "headers"),
+        pytest.param(
+            "!#$&'(",
+            "keykey2",
+            "query",
+            marks=[
+                pytest.mark.xfail,
+                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-10762"),
+                pytest.mark.fuzz,
+            ],
+        ),
+        pytest.param(
+            ")*+,-./:",
+            "keykey3",
+            "query",
+            marks=[
+                pytest.mark.xfail,
+                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-10762"),
+                pytest.mark.fuzz,
+            ],
+        ),
+        pytest.param(
+            ";=?@",
+            "keykey4",
+            "query",
+            marks=[
+                pytest.mark.xfail,
+                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-10762"),
+                pytest.mark.fuzz,
+            ],
+        ),
+        pytest.param(
+            "_~ID",
+            "keykey5",
+            "query",
+            marks=[
+                pytest.mark.fuzz,
+            ],
+        ),
+        pytest.param(
+            '"%<>[\\]^`{|}',
+            "keykey6",
+            "query",
             marks=[
                 pytest.mark.xfail,
                 pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-10762"),
@@ -105,24 +192,35 @@ def app_key(request):
         pytest.param(
             "{}*~KEY",
             "keykey7",
-            marks=[pytest.mark.xfail, pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-10761")],
+            "query",
+            marks=[
+                pytest.mark.xfail,
+                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-10761"),
+                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-10762"),
+                pytest.mark.fuzz,
+            ],
         ),
         pytest.param(
             "9999_",
             "keykey8",
+            "query",
             marks=[
-                pytest.mark.issue("https://issues.redhat.com/browse/THREESCALE-10763"),
                 pytest.mark.fuzz,
             ],
         ),
     ],
     indirect=True,
 )
-def test_successful_requests(client2, app_id, app_key):
+def test_successful_requests(client2, app_id, app_key, credentials_location):
     """Test checks if applications was created and is functional"""
-    response = client2.get("/")
+    response = client2.get("/get")
     assert response.status_code == 200
 
     echoed_request = EchoedRequest.create(response)
-    assert echoed_request.params["app_key"] == app_key
-    assert echoed_request.params["app_id"] == app_id
+    # import pdb; pdb.set_trace()
+    if credentials_location == "headers":
+        assert echoed_request.headers["app_key"] == app_key
+        assert echoed_request.headers["app_id"] == app_id
+    else:
+        assert echoed_request.params["app_key"] == app_key
+        assert echoed_request.params["app_id"] == app_id
