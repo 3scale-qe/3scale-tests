3439a4a81d0760f83fe83b9b5edc09fdaa9a677e
Marek Stastny <mstastny@redhat.com>
2026-03-05 14:06:20 +0100

    Add mailhog cleanup after test_message_counter

--- testsuite/tests/ui/mail_and_messages/test_message_counter.py
+++ testsuite/tests/ui/mail_and_messages/test_message_counter.py
@@ -4,6 +4,7 @@ import pytest
 
 from packaging.version import Version  # noqa # pylint: disable=unused-import
 from testsuite import TESTED_VERSION  # noqa # pylint: disable=unused-import
+from testsuite.utils import blame
 from testsuite.ui.views.admin.audience.messages import MessagesView
 from testsuite.ui.views.admin.foundation import DashboardView
 from testsuite.ui.views.devel.messages import ComposeView
@@ -26,10 +27,24 @@ def send_message_from_devel(navigator, subject, body):
     devel_compose_view.send_message(subject, body)
 
 
+@pytest.fixture()
+def subjects(request):
+    """Create list of message subjects"""
+    return [blame(request, f"subject-{i}") for i in range(2)]
+
+
+@pytest.fixture()
+def contents(request):
+    """Create list of message bodies"""
+    return [blame(request, f"content-{i}") for i in range(2)]
+
+
 # pylint: disable=too-many-arguments
 @pytest.mark.skipif("TESTED_VERSION < Version('2.15')")
 @pytest.mark.usefixtures("login", "application", "service")
-def test_message_counter(custom_devel_login, custom_admin_login, account, navigator):
+def test_message_counter(
+    custom_devel_login, custom_admin_login, account, navigator, mailhog_client, subjects, contents
+):
     """
     Test:
         - Take a note of number of messages (msg_count) and unread messages (unread_count) in messages tab in
@@ -46,23 +61,28 @@ def test_message_counter(custom_devel_login, custom_admin_login, account, naviga
     start_unread_msg_count = admin_dashboard_view.unread_msg_count
 
     custom_devel_login(account)
-    send_message_from_devel(navigator, "subject1", "body1")
-    send_message_from_devel(navigator, "subject2", "body2")
+    try:
+        send_message_from_devel(navigator, subjects[0], contents[0])
+        send_message_from_devel(navigator, subjects[1], contents[1])
+    finally:
+        mailhog_client.find_message(
+            subject=f"New message from {account.entity_name}"
+        )  # assure that messages will be deleted if skip_cleanup is false
 
     custom_admin_login(account)
     assert_dashboard_counters(navigator, start_msg_count + 2, start_unread_msg_count + 2)
 
     admin_messages_view = navigator.navigate(MessagesView)
-    link = admin_messages_view.get_unread_msg_link(Subject="subject1", From=account.entity_name)
+    link = admin_messages_view.get_unread_msg_link(Subject=subjects[0], From=account.entity_name)
     link.click()
     admin_messages_view = navigator.navigate(MessagesView)
-    link = admin_messages_view.get_unread_msg_link(Subject="subject2", From=account.entity_name)
+    link = admin_messages_view.get_unread_msg_link(Subject=subjects[1], From=account.entity_name)
     link.click()
 
     assert_dashboard_counters(navigator, start_msg_count + 2, start_unread_msg_count + 0)
 
     admin_messages_view = navigator.navigate(MessagesView)
-    admin_messages_view.delete_message(Subject="subject1", From=account.entity_name)
-    admin_messages_view.delete_message(Subject="subject2", From=account.entity_name)
+    admin_messages_view.delete_message(Subject=subjects[0], From=account.entity_name)
+    admin_messages_view.delete_message(Subject=subjects[1], From=account.entity_name)
 
     assert_dashboard_counters(navigator, start_msg_count + 0, start_unread_msg_count + 0)
