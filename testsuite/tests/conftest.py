d085477c8993bb3cbd2acc30cab0bbaae852ead8
cathal-bailey <cathal.bailey@ibm.com>
2026-09-09 15:15:10 +0100

    Removing minio from testsuite/tests/conftest.py

--- testsuite/tests/conftest.py
+++ testsuite/tests/conftest.py
@@ -312,8 +312,6 @@ def pytest_metadata(metadata):
         "go-httpbin+ssl",
         "jaeger-query",
         "jaeger-query+ssl",
-        "minio",
-        "minio+ssl",
         "mockserver",
         "mockserver+ssl",
     ]
